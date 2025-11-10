cdbfbc9fa30970911dcda4ea8a0e7f8ca03e966a
fotokorotun <62463938+fotokorotun@users.noreply.github.com>
2025-11-10 12:51:02 +0100

    Delete Sprint_2 directory

--- Sprint_2/Nivel_1/Sergii_Korotun_Sprint_2(nivel1_2.1).sql
+++
@@ -1,4 +0,0 @@
-SELECT DISTINCT c.country
-FROM transaction t
-JOIN company c ON t.company_id = c.id
-WHERE t.declined = FALSE;
--- Sprint_2/Nivel_1/Sergii_Korotun_Sprint_2(nivel1_2.2).sql
+++
@@ -1,4 +0,0 @@
-SELECT COUNT(DISTINCT c.country) AS total_paisos
-FROM transaction t
-JOIN company c ON t.company_id = c.id
-WHERE t.declined = FALSE;
